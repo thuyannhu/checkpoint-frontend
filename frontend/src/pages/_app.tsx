3e2a74c17e90c8923458227552b0e7f025e84b81
thuyannhu <nhu.thuy-an@hotmail.fr>
2024-05-13 11:50:50 +0200

    added link

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -4,23 +4,32 @@ import {
 	ApolloClient,
 	InMemoryCache,
 	ApolloProvider,
+	createHttpLink,
 } from "@apollo/client";
 import type { AppProps } from "next/app";
 import dynamic from "next/dynamic";
 
+const backend_url = "http://localhost:4000/graphql";
+
+const httpLink = createHttpLink({
+	uri: backend_url,
+});
+
 const client = new ApolloClient({
+	link: httpLink,
 	cache: new InMemoryCache(),
 });
 
 function App({ Component, pageProps }: AppProps) {
-  return (
+	return (
 		<>
 			<ApolloProvider client={client}>
 				<Header />
 				<Component {...pageProps} />
 			</ApolloProvider>
 		</>
-	);}
+	);
+}
 
 // Disabling SSR
 export default dynamic(() => Promise.resolve(App), { ssr: false });
